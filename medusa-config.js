230c56b18b0557a62fcd7dc93c3e88183f699f7e
Jay Linsell <holla@jaylinsell.com>
2024-02-21 20:19:11 +1100

    actually added stripe to config

--- medusa-config.js
+++ medusa-config.js
@@ -37,6 +37,14 @@ const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
   {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+      payment_description: 'Ocean Reach Brewing',
+    },
+  },
+  {
     resolve: `@medusajs/file-local`,
     options: {
       upload_dir: "uploads",
